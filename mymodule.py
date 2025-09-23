9da12ac778e3865ad2cbbc8799d3ac27f30fbdb0
azryung <azryung@gmail.com>
2025-09-23 16:11:55 +0900

    class 만듬

--- mymodule.py
+++ mymodule.py
@@ -4,3 +4,18 @@ def my_add(a,b):
 def my_sub(a,b):
     return a-b
 
+
+
+class Character:
+    def __init__(self,name):
+        self.name = name
+    def speak(self):
+        print(f'저는 {self.name}입니다.')
+    
+print(f'mymodule 안에서의 __name__:{__name__}')
+
+if __name__ == "__main__":  ##__name__은 파이선에서 정해둔 변수이름.
+    print(my_add(3,4))
+    print(my_sub(6,5))
+    wizard = Character('법사')
+    wizard.speak()
